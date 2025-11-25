0a2f8c37bdb6f1a3bb9f51034a6e2ff95916963a
Anand Muley <92146687+anandmuley568@users.noreply.github.com>
2025-11-26 01:19:01 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -10,18 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
-	public String welcome() {
-		return "welcome to javatechie !";
+	public String welcome(){
+		return "Welcome to javatechie !";
 	}
-	
-//	echo "# github-action-example" >> README.md
+
+//	echo "# github-actions-example" >> README.md
 //	git init
 //	git add README.md
 //	git commit -m "first commit"
 //	git branch -M main
-//	git remote add origin git@github.com:anandmuley568/github-action-example.git
+//	git remote add origin https://github.com/Java-Techie-jt/github-actions-example.git
 //	git push -u origin main
-	
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
 	}
